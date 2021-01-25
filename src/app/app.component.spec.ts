e07e90c4e3b0e6c7e2b6a023e3875d2b1ec9642f
unknown <namratadpatil410@gmail.com>
2021-01-25 11:46:08 +0530

    namrata

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ang-project1'`, () => {
+  it(`should have as title 'OnBoarding-Pro'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ang-project1');
+    expect(app.title).toEqual('OnBoarding-Pro');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('ang-project1 app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('OnBoarding-Pro app is running!');
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ang-project1';
+  title = 'OnBoarding-Pro';
 }
